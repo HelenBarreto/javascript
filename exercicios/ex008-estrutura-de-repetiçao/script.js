ad65b2a8cb25f6c75bf7726a1da590ad2615ca8a
Helen Barreto <helenmendesbarreto@gmail.com>
2023-03-28 19:03:52 -0300

    atualizei

---
+++ exercicios/ex008-estrutura-de-repetiçao/script.js
@@ -0,0 +1,21 @@
+var c = 1
+
+while ( c <= 6){
+    console.log('bom dia!')
+    c++;
+   
+
+}
+
+var b = 3
+do{
+    console.log(`passo ${b}`)
+    b++
+}while (b <= 20)
+
+
+
+
+for(var f = 7; f >= 10; f++){
+    console.log('f')
+}
